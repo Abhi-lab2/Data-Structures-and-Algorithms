58e7c2c711aaec79e5fe35ce7a2a2ac99fabde2f
Abhijeet Chaudhari <abhijeetchaudhari615@gmail.com>
2022-09-22 00:55:27 +0530

    swap two numbers

---
+++ swapTwoNumbers.js
@@ -0,0 +1,10 @@
+//  Swapping two numbers without using extra space and extra variable.
+
+let a = 2;
+let b = 3;
+
+a = a + b;
+b = a - b;
+a = a - b;
+
+console.log("a:", a + " " + "b:", b); // a : 3 and b : 2
